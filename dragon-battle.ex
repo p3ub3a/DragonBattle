7f0b0f3582c015c40c9eab9cd1906bdcaf3804a5
Petru <petrubodea94@gmail.com>
2020-05-14 11:43:51 +0300

    some refactoring

--- dragon-battle.ex
+++ dragon-battle.ex
@@ -3,83 +3,111 @@ defmodule Battle do
         necromancerHp = 10000
         dragonHp = 1000000
 
-        parent = self()
+        parentProcess = self()
 
-        # dragonProcess = spawn(&(Dragon.useWhiptail/2)) ??
+        dragonProcess = spawn(Dragon, :battle, [necromancerHp, parentProcess])
+        dsp = spawn(DragonStrategy, :useWhiptail, [dragonProcess])
 
-        dsp = spawn(DragonStrategy, :battleNecromancer, [necromancerHp, parent])
-        nsp = spawn(NecromancerStrategy, :battleDragon, [dragonHp, parent])
+        necromancerProcess = spawn(Necromancer, :battle, [dragonHp, parentProcess])
+        nsp = spawn(NecromancerStrategy, :useAntiZombieBolt, [necromancerProcess])
 
         receive do
             {:gameOver, message } -> 
-                Process.exit(dsp, :gameOver)
-                Process.exit(nsp, :gameOver)
+                Process.exit(dragonProcess, :gameOver)
+                Process.exit(dragonStrategyProcess, :gameOver)
+                Process.exit(necromancerProcess, :gameOver)
+                Process.exit(necromancerStrategyProcess, :gameOver)
                 IO.puts "#{message}"
         end
     end
 
-    def dealDamage(targetHp, min, max) do
-        remainingHp = targetHp - Enum.random(min..max)
-    end
-end
-
-defmodule DragonStrategy do
-    def battleNecromancer(necromancerHp, parent) do
-
-        dragonStrategyProcess = self()
-
-        spawn(Dragon, :useWhiptail, [necromancerHp, dragonStrategyProcess])
-
-        receive do
-            {:action, targetHp} -> 
-                if targetHp <= 0 do 
-                    send(parent, {:gameOver, "The dragon won the battle!"})
-                else
-                    IO.puts "The dragon used Whip Tail, necromancer has #{targetHp} HP remaining"
-                    battleNecromancer(targetHp, parent) 
-                end
-        # after
-        #     5 -> :timeout
-        end
-    end
-end
-
-defmodule NecromancerStrategy do
-    def battleDragon(dragonHp, parent) do
-
-        necromancerStrategyProcess = self()
-
-        spawn(Necromancer, :useAntiZombieBolt, [dragonHp, necromancerStrategyProcess])
-
-        receive do
-            {:action, targetHp} -> 
-                if targetHp <= 0 do 
-                    send(parent, {:gameOver, "The necromancer won the battle!"})
-                else
-                    IO.puts "The necromancer used Anti Zombie Bolt, dragon has #{targetHp} HP remaining"
-                    battleDragon(targetHp, parent) 
-                end
-        # after
-        #     12 -> :timeout
-        end
+    def getDamage(min, max) do
+        damage = Enum.random(min..max)
     end
 end
 
 defmodule Dragon do
-    def useWhiptail(targetHp, process) do
-        dealDamage = &(Battle.dealDamage/3)
-        targetHp = dealDamage.(targetHp, 50, 100)
-        # targetHp = Battle.dealDamage(targetHp, 50, 100)
-        Process.sleep(5)
-        send(process, {:action, targetHp}) 
+    def battle(dragonHp, parentProcess) do
+        process = self()
+        # enemyList = 
+        
+        receive do
+            {:info, damageTaken} ->
+                if dragonHp <= 0 do 
+                    send(parentProcess, {:gameOver, "The necromancer won the battle!"})
+                else
+                    dragonHp = dragonHp - damageTaken
+                    IO.puts "The dragon took #{damageTaken} damage"
+                end
+                battle(dragonHp, parentProcess)
+            {:characterDead, characterName} ->
+
+            # -------------------- skills --------------------
+            {:whiptail, skillName} ->
+                dealDamage = &(Battle.getDamage/2)
+                # ???
+                send(necromancerProcess, {:info, getDamage.(50, 100)})
+                # 
+                battle(dragonHp, parentProcess)
+            {:dragonBreath, skillName} ->
+
+        end
     end
 end
 
 defmodule Necromancer do
-    def useAntiZombieBolt(targetHp, process) do
-        dealDamage = &(Battle.dealDamage/3)
-        targetHp = dealDamage.(targetHp, 0, 1000)
+    def battle(necromancerHp, parentProcess) do
+        process = self()
+        # enemyList = 
+        
+        receive do
+            {:info, damageTaken} ->
+                if necromancerHp <= 0 do 
+                    send(parentProcess, {:gameOver, "The dragon won the battle!"})
+                else
+                    necromancerHp = necromancerHp - damageTaken
+                    IO.puts "The necromancer took #{damageTaken} damage"
+                end
+                battle(necromancerHp, parentProcess)
+            {:characterDead, characterName} ->
+
+            # -------------------- skills --------------------
+            {:antiZombieBolt, skillName} ->
+                dealDamage = &(Battle.getDamage/2)
+                # ???
+                send(dragonProcess, {:info, getDamage.(0, 1000)})
+                # 
+                battle(necromancerHp, parentProcess)
+            {:zombieKnight, skillName} ->
+                
+        end
+    end
+end
+
+defmodule DragonStrategy do
+    def useWhiptail(dragonProcess) do
+        send(dragonProcess, {:whiptail, "whiptail"}) 
+        Process.sleep(5)
+        useWhiptail(dragonProcess)
+    end
+end
+
+defmodule NecromancerStrategy do
+    def useAntiZombieBolt(necromancerProcess) do
+        send(necromancerProcess, {:antiZombieBolt, "anti zombie bolt"}) 
         Process.sleep(12)
-        send(process, {:action, targetHp}) 
+        useAntiZombieBolt(necromancerProcess)
+    end
+
+    def summonZombieKnight(necromancerProcess) do
+        send(necromancerProcess, {:zombieKnight, "summon zombie knight"}) 
+        Process.sleep(20)
+        summonZombieKnight(necromancerProcess)
+    end
+end
+
+defmodule ZombieKnight do
+    receive do
+            
     end
 end
